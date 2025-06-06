6f8b5a10c0142c34a8a09dc349ca6f56dfb208f1
Baiur Yuliia <juliy14497@gmail.com>
2025-06-06 22:20:39 +0300

    Added authorization and registration

--- src/cosmetics_shop/urls.py
+++ src/cosmetics_shop/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_page, name='product_page'),
     path('brand/', views.brand_page, name='brand_page'),
     path('brand/<int:brand_id>', views.brand_products, name='brand_detail'),
+    path('register/', views.register, name='register'),
     path('', views.main_page, name='main_page'),
 ]
