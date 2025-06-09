849bf8db767ab9043362e03c6cb6c95c9df8392c
Baiur Yuliia <juliy14497@gmail.com>
2025-06-09 18:24:06 +0300

    Added new models - Card, CardItem, Order, OrderItem; Changed other models; Changed files due to model changes

--- src/cosmetics_shop/admin.py
+++ src/cosmetics_shop/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from cosmetics_shop.models import Category, GroupProduct, Product, Brand
+from cosmetics_shop.models import Category, GroupProduct, Product, Brand, Card, CardItem, Order, OrderItem
 
 admin.site.register(Category)
 admin.site.register(GroupProduct)
 admin.site.register(Product)
 admin.site.register(Brand)
+admin.site.register(Card)
+admin.site.register(CardItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- src/cosmetics_shop/views.py
+++ src/cosmetics_shop/views.py
@@ -29,7 +29,7 @@ def main_page(request):
 def category_page(request, category_id):
     category = Category.objects.filter(id=category_id)
     group_product = GroupProduct.objects.filter(category=category_id)
-    product = Product.objects.filter(group_product__in=group_product)
+    product = Product.objects.filter(group__in=group_product)
     return render(request, 'cosmetics_shop/category_page.html', {
         'title': 'Category',
         'category': category,
@@ -40,7 +40,7 @@ def category_page(request, category_id):
 
 def group_page(request, group_id):
     group_product = GroupProduct.objects.filter(id=group_id)
-    product = Product.objects.filter(group_product__in=group_product)
+    product = Product.objects.filter(group__in=group_product)
     return render(request, 'cosmetics_shop/category_page.html', {
         'title': 'Category',
         'group_product': group_product,
