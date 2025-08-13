2eb60652dd4b1b080479f37a085f90c9bb6c810d
Baiur Yuliia <juliy14497@gmail.com>
2025-08-13 19:43:19 +0300

    Change price filters

--- src/cosmetics_shop/templatetags/custom_filters.py
+++ src/cosmetics_shop/templatetags/custom_filters.py
@@ -10,3 +10,12 @@ def multiply(value, arg):
         return int(value) * int(arg)
     except (ValueError, TypeError):
         return ""
+
+
+@register.filter
+def number_format(value):
+    try:
+        return f"{float(value):,.2f}".replace(",", " ").replace(".", ",")
+    except (ValueError, TypeError):
+        return value
+
