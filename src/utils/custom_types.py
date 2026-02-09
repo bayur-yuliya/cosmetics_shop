4e80593cba400bc7a8de23c35644ab58f93d3297
Baiur Yuliia <juliy14497@gmail.com>
2026-02-09 16:42:53 +0200

    refactor: remove unuse function

--- src/utils/custom_types.py
+++ src/utils/custom_types.py
@@ -1,5 +1,3 @@
-from typing import TypeGuard
-from django.contrib.auth.models import AnonymousUser
 from django.http import HttpRequest
 
 from accounts.models import CustomUser
@@ -7,7 +5,3 @@ from accounts.models import CustomUser
 
 class AuthenticatedRequest(HttpRequest):
     user: CustomUser
-
-
-def is_fully_authenticated(user: CustomUser | AnonymousUser) -> TypeGuard[CustomUser]:
-    return user.is_authenticated
