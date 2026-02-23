aea7622b0fa1438b6f034992d8b5a46102666fcd
Baiur Yuliia <juliy14497@gmail.com>
2026-02-23 21:48:09 +0200

    refactor: merging creation and modification files

--- src/staff/services/list_service.py
+++ src/staff/services/list_service.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DeleteView
 
 from config.settings import PRODUCTS_PER_PAGE
 from staff.mixins import (
@@ -23,16 +23,10 @@ class BaseStaffListView(
     context_object_name = "objects"
 
 
-class BaseStaffCreateView(
-    PageTitleMixin, LoginRequiredMixin, StaffPermissionExceptionMixin, CreateView
+class BaseStaffManageView(
+    PageTitleMixin, LoginRequiredMixin, StaffPermissionExceptionMixin
 ):
-    template_name = "staff/catalog/create_page.html"
-
-
-class BaseStaffChangeView(
-    PageTitleMixin, LoginRequiredMixin, StaffPermissionExceptionMixin, UpdateView
-):
-    template_name = "staff/catalog/update_page.html"
+    template_name = "staff/catalog/manage_page.html"
 
 
 class BaseStaffDeleteView(
